b38dee1ce44197a6cd59ddac1204e74bc171b4be
Lite McFish <lite260@gmail.com>
2016-01-18 18:27:23 -0600

    removed output. made programs restart

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 var spawn = require('child_process').spawn;
 var fs = require('fs');
-console.log(tag() + "Reading startup file... (/etc/startup.json)")
 fs.readFile('/etc/startup.json', 'utf8', function (err, data) {
     var config;
     if (err) { // doesn't exist
         config = {
             commands: []
         };
-        
-        fs.writeFile("/etc/startup.json", JSON.stringify(config), function(err) {
-            if(err) {
+
+        fs.writeFile("/etc/startup.json", JSON.stringify(config), function (err) {
+            if (err) {
                 return console.log(err);
             }
-            
-            console.log(tag + "Saved blank config file.");
-        }); 
+        });
     } else {
         config = JSON.parse(data);
     }
-    
+
     var l = config.commands.length;
-    for(var p = 0; p < l; p++) {
+    for (var p = 0; p < l; p++) {
         console.log(tag() + "Executing: " + config.commands[p].cmd + config.commands[p].args.join(" "));
-        spawn(config.commands[p].cmd, config.commands[p].args);
+        launch(config.commands[p].cmd, config.commands[p].args); // launch the program
     }
-    
-    console.log(tag() + "All programs launched.");
 });
 
-function tag() {
-    return "[PalmTree] ";
+function launch(cmd, args) {
+    var ls = spawn(cmd, args);
+    ls.on('exit', (code) {
+        launch(cmd, args); // re-launch silently on exit
+    });
 }
