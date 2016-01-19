18cddd506e809c546c3c71bc415de9b1412a3a98
Lite <McFish>
2016-01-19 13:44:13 -0600

    use clean for loop and remove errors and fs

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var spawn = require('child_process').spawn;
-var fs = require('fs');
 var config = require('/etc/startup.json');
-var length = config.commands.length;
-for (var p = 0; p < length; p++) {
-    if (!config.commands[p].cmd) {
-        throw ("No command specified for #" + (p + 1) + " in /etc/startup.json.");
-    } else {
-        launch(config.commands[p].cmd, config.commands[p].args); // launch the program
-    }
+for (var p in config.commands) {
+    launch(config.commands[p].cmd, config.commands[p].args); // launch the program
 }
 
 function launch(cmd, args) {
